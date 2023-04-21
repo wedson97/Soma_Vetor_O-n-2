2e8bc171adf3098f34a47dbfe61c28bb1a32a604
Wedson Silva <112664580+wedson97@users.noreply.github.com>
2023-04-20 22:19:39 -0300

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,27 @@
+package myproject.vetor;
+public class TwoSum {
+    private int[] entrada;
+    private int target;
+    
+    public TwoSum(int[] numbers, int target){
+        this.entrada = numbers;
+        this.target = target;
+    }
+    public void calcular(){
+        int[] saida =  new int[2];
+        int qtd=0;
+        for(int i = 0;i<entrada.length;i++){
+            for(int j = 1;j<entrada.length;j++){
+                if(entrada[i]+entrada[j]==this.target){
+                    saida[qtd++]=i;
+                    if(qtd<saida.length){
+                        saida[qtd]=j;
+                    }
+                }
+            }
+        }
+        for(int i = 0;i<saida.length;i++){
+            System.out.print(saida[i]+" "); 
+        }
+    }
+}
